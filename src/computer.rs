646c3ddcf0e7429f68e77255f9eb39c0b539e2ca
nbrass <nbrass@uw.edu>
2022-08-14 21:06:00 -0700

    add a bunch of instructions

--- src/computer.rs
+++ src/computer.rs
@@ -36,18 +36,81 @@ impl Computer {
         loop {
             match self.nums[self.cursor] {
                 0 => return ExitCode::Halt,
-                19 => {
-                    self.output.push(
-                        self.nums[self.cursor + 1]
-                            .try_into()
-                            .expect("not valid ascii"),
-                    );
-                    self.cursor += 2;
-                }
-                21 => self.cursor += 1,
+                1 => self.set_op(),
+                2 => self.push_op(),
+                3 => self.pop_op(),
+                4 => self.eq_op(),
+                5 => self.gt_op(),
+                6 => self.jmp_op(),
+                7 => self.jt_op(),
+                8 => self.jf_op(),
+                9 => self.add_op(),
+                10 => self.mult_op(),
+                11 => self.mod_op(),
+                12 => self.and_op(),
+                13 => self.or_op(),
+                14 => self.not_op(),
+                15 => self.rmem_op(),
+                16 => self.wmem_op(),
+                17 => self.call_op(),
+                18 => self.ret_op(),
+                19 => self.out_op(),
+                20 => self.in_op(),
+                21 => self.no_op(),
                 _ => return ExitCode::InvalidOperation,
             }
         }
+    }
+
+    fn set_op(&mut self) {}
+
+    fn push_op(&mut self) {}
+
+    fn pop_op(&mut self) {}
+
+    fn eq_op(&mut self) {}
+
+    fn gt_op(&mut self) {}
+
+    fn jmp_op(&mut self) {}
+
+    fn jt_op(&mut self) {}
+
+    fn jf_op(&mut self) {}
+
+    fn add_op(&mut self) {}
+
+    fn mult_op(&mut self) {}
+
+    fn mod_op(&mut self) {}
+
+    fn and_op(&mut self) {}
+
+    fn or_op(&mut self) {}
+
+    fn not_op(&mut self) {}
+
+    fn rmem_op(&mut self) {}
+
+    fn wmem_op(&mut self) {}
+
+    fn call_op(&mut self) {}
+
+    fn ret_op(&mut self) {}
+
+    fn out_op(&mut self) {
+        self.output.push(
+            self.nums[self.cursor + 1]
+                .try_into()
+                .expect("not valid ascii"),
+        );
+        self.cursor += 2;
+    }
+
+    fn in_op(&mut self) {}
+
+    fn no_op(&mut self) {
+        self.cursor += 1
     }
 
     pub fn get_output(&self) -> &[u8] {
