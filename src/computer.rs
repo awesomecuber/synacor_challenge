f50159f09bd6f3808bea32ff06c9ea0895efe6b6
nbrass <nbrass@uw.edu>
2022-08-17 01:42:11 -0700

    get working cli thing

--- src/computer.rs
+++ src/computer.rs
@@ -1,10 +1,11 @@
-use anyhow::Result;
-use std::{path::Path, str};
+use std::{collections::VecDeque, str};
 
 pub enum ExitCode {
+    Continue,
     Halt,
     InvalidOperation,
     OutOfOperations,
+    NeedInput,
 }
 
 pub struct Computer {
@@ -13,25 +14,34 @@ pub struct Computer {
     output: Vec<u8>,
     registers: [u16; 8],
     stack: Vec<u16>,
+    input: VecDeque<u8>,
 }
 
 impl Computer {
-    pub fn from_file(file: impl AsRef<Path>) -> Result<Computer> {
-        let nums: Vec<u16> = std::fs::read(file)?
+    pub fn from_bytes(bytes: &[u8]) -> Computer {
+        let nums: Vec<u16> = bytes
             .chunks_exact(2)
             .map(|n| u16::from_le_bytes(n.try_into().unwrap()))
             .collect();
-        Ok(Computer::new(&nums))
+        Computer::new(&nums)
     }
 
     pub fn new(nums: &[u16]) -> Computer {
+        let mut mem = nums.to_vec();
+        mem.resize((1 << 15) - 1, 0);
         Computer {
-            mem: nums.to_vec(),
+            mem,
             cursor: 0,
-            output: vec![],
+            output: Vec::new(),
             registers: [0; 8],
-            stack: vec![],
+            stack: Vec::new(),
+            input: VecDeque::new(),
         }
+    }
+
+    pub fn add_input(&mut self, line: &[u8]) {
+        self.input.extend(line);
+        self.input.push_back('\n'.try_into().unwrap());
     }
 
     pub fn read_instructions_until_terminate(&mut self) -> ExitCode {
@@ -39,8 +49,8 @@ impl Computer {
             if self.cursor >= self.mem.len() {
                 break;
             }
-            match self.mem[self.cursor] {
-                0 => return ExitCode::Halt,
+            let exit_code = match self.mem[self.cursor] {
+                0 => ExitCode::Halt,
                 1 => self.set_op(),
                 2 => self.push_op(),
                 3 => self.pop_op(),
@@ -62,121 +72,177 @@ impl Computer {
                 19 => self.out_op(),
                 20 => self.in_op(),
                 21 => self.no_op(),
-                _ => return ExitCode::InvalidOperation,
+                _ => ExitCode::InvalidOperation,
+            };
+            match exit_code {
+                ExitCode::Continue => {}
+                _ => return exit_code,
             }
         }
         ExitCode::OutOfOperations
     }
 
-    fn set_op(&mut self) {
+    fn set_op(&mut self) -> ExitCode {
         let (reg, val) = self.get_two_arguments();
         self.set_reg(reg, val);
+        ExitCode::Continue
     }
 
-    fn push_op(&mut self) {
+    fn push_op(&mut self) -> ExitCode {
         let val = self.get_one_argument();
         self.stack.push(self.read(val));
+        ExitCode::Continue
     }
 
-    fn pop_op(&mut self) {
+    fn pop_op(&mut self) -> ExitCode {
         let dest = self.get_one_argument();
         let val = self.stack.pop().expect("must have item in stack");
         self.set(dest, val);
+        ExitCode::Continue
     }
 
-    fn eq_op(&mut self) {
+    fn eq_op(&mut self) -> ExitCode {
         let (dest, left, right) = self.get_three_arguments();
         self.set(dest, (self.read(left) == self.read(right)) as u16);
+        ExitCode::Continue
     }
 
-    fn gt_op(&mut self) {
+    fn gt_op(&mut self) -> ExitCode {
         let (dest, left, right) = self.get_three_arguments();
         self.set(dest, (self.read(left) > self.read(right)) as u16);
+        ExitCode::Continue
     }
 
-    fn jmp_op(&mut self) {
+    fn jmp_op(&mut self) -> ExitCode {
         let jump_to = self.get_one_argument();
         self.cursor = self.read(jump_to) as usize;
+        ExitCode::Continue
     }
 
-    fn jt_op(&mut self) {
+    fn jt_op(&mut self) -> ExitCode {
         let (val, jump_to) = self.get_two_arguments();
         if self.read(val) != 0 {
             self.cursor = self.read(jump_to) as usize;
         }
+        ExitCode::Continue
     }
 
-    fn jf_op(&mut self) {
+    fn jf_op(&mut self) -> ExitCode {
         let (val, jump_to) = self.get_two_arguments();
         if self.read(val) == 0 {
             self.cursor = self.read(jump_to) as usize;
         }
+        ExitCode::Continue
     }
 
-    fn add_op(&mut self) {
+    fn add_op(&mut self) -> ExitCode {
         let (dest, left, right) = self.get_three_arguments();
-        self.set_reg(dest, (self.read(left) + self.read(right)) % 32768);
+        self.set_reg(
+            dest,
+            u16::wrapping_add(self.read(left), self.read(right)) % (1 << 15),
+        );
+        ExitCode::Continue
     }
 
-    fn mult_op(&mut self) {
-        todo!()
+    fn mult_op(&mut self) -> ExitCode {
+        let (dest, left, right) = self.get_three_arguments();
+        self.set_reg(
+            dest,
+            u16::wrapping_mul(self.read(left), self.read(right)) % (1 << 15),
+        );
+        ExitCode::Continue
     }
 
-    fn mod_op(&mut self) {
-        todo!()
+    fn mod_op(&mut self) -> ExitCode {
+        let (dest, left, right) = self.get_three_arguments();
+        self.set_reg(dest, self.read(left) % self.read(right));
+        ExitCode::Continue
     }
 
-    fn and_op(&mut self) {
+    fn and_op(&mut self) -> ExitCode {
         let (dest, left, right) = self.get_three_arguments();
         self.set(dest, self.read(left) & self.read(right));
+        ExitCode::Continue
     }
 
-    fn or_op(&mut self) {
+    fn or_op(&mut self) -> ExitCode {
         let (dest, left, right) = self.get_three_arguments();
         self.set(dest, self.read(left) | self.read(right));
+        ExitCode::Continue
     }
 
-    fn not_op(&mut self) {
+    fn not_op(&mut self) -> ExitCode {
         let (dest, val) = self.get_two_arguments();
-        self.set(dest, !self.read(val));
+        let first_bit = 1 << 15;
+        let flipped = if self.read(val) < first_bit {
+            !self.read(val) - first_bit
+        } else {
+            !self.read(val) + first_bit
+        };
+        self.set(dest, flipped);
+        ExitCode::Continue
     }
 
-    fn rmem_op(&mut self) {
-        todo!()
+    fn rmem_op(&mut self) -> ExitCode {
+        let (dest, val) = self.get_two_arguments();
+        self.set(dest, self.mem[self.read(val) as usize]);
+        ExitCode::Continue
     }
 
-    fn wmem_op(&mut self) {
-        todo!()
+    fn wmem_op(&mut self) -> ExitCode {
+        let (dest, val) = self.get_two_arguments();
+        self.set(self.read(dest), self.read(val));
+        ExitCode::Continue
     }
 
-    fn call_op(&mut self) {
-        todo!()
+    fn call_op(&mut self) -> ExitCode {
+        let jump_to = self.get_one_argument();
+        self.stack.push(self.cursor as u16);
+        self.cursor = self.read(jump_to) as usize;
+        ExitCode::Continue
     }
 
-    fn ret_op(&mut self) {
-        todo!()
+    fn ret_op(&mut self) -> ExitCode {
+        self.get_no_arguments();
+        match self.stack.pop() {
+            Some(addr) => self.cursor = addr as usize,
+            None => return ExitCode::Halt,
+        }
+        ExitCode::Continue
     }
 
-    fn out_op(&mut self) {
+    fn out_op(&mut self) -> ExitCode {
         let char = self.get_one_argument();
         self.output
             .push(self.read(char).try_into().expect("not valid ascii"));
+        ExitCode::Continue
     }
 
-    fn in_op(&mut self) {
-        todo!()
+    fn in_op(&mut self) -> ExitCode {
+        let dest = self.get_one_argument();
+        match self.input.pop_front() {
+            Some(char) => self.set(dest, char as u16),
+            None => {
+                self.cursor -= 2; // get back in original state
+                return ExitCode::NeedInput;
+            }
+        }
+        ExitCode::Continue
     }
 
-    fn no_op(&mut self) {
+    fn no_op(&mut self) -> ExitCode {
         self.get_no_arguments();
+        ExitCode::Continue
     }
 
     fn read(&self, num: u16) -> u16 {
-        assert!(num <= 32775, "num is invalid");
         if num < 32768 {
             return num;
         }
-        self.registers[(num % 32768) as usize]
+        if num <= 32775 {
+            return self.registers[(num % 32768) as usize];
+        }
+        0
     }
 
     fn get_no_arguments(&mut self) {
@@ -205,8 +271,8 @@ impl Computer {
     fn set(&mut self, dest: u16, val: u16) {
         if dest >= 32768 {
             self.set_reg(dest, val)
-        } else if self.mem.len() <= dest as usize {
-            self.mem.resize((dest + 1) as usize, 0);
+        } else {
+            self.mem[dest as usize] = val;
         }
     }
 
@@ -219,7 +285,9 @@ impl Computer {
         &self.output
     }
 
-    pub fn _get_output_as_string(&self) -> Result<String> {
-        Ok(str::from_utf8(&self.output)?.to_owned())
+    pub fn get_output_as_string(&mut self) -> String {
+        let to_return = str::from_utf8(&self.output).unwrap().to_owned();
+        self.output = Vec::new();
+        to_return
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
-use std::fs::write;
+use std::io::stdin;
 
 use anyhow::Result;
 use computer::Computer;
 
+use crate::computer::ExitCode;
+
 mod computer;
 
-fn main() -> Result<()> {
-    let mut comp = Computer::from_file("challenge.bin")?;
-    // let mut comp = Computer::new(&[9, 32768, 32769, 65, 19, 32768]);
-    match comp.read_instructions_until_terminate() {
-        computer::ExitCode::Halt => println!("Computer halted successfully"),
-        computer::ExitCode::InvalidOperation => println!("Computer encountered invalid operation"),
-        computer::ExitCode::OutOfOperations => println!("Computer ran out of operations"),
+fn main() {
+    let mut comp = Computer::from_bytes(include_bytes!("../challenge.bin"));
+
+    comp.read_instructions_until_terminate();
+    println!("{}", comp.get_output_as_string());
+    loop {
+        let result = comp.read_instructions_until_terminate();
+        println!("{}", comp.get_output_as_string());
+        match result {
+            ExitCode::Continue => {
+                println!("Computer not complete");
+                break;
+            }
+            ExitCode::Halt => {
+                println!("Computer halted successfully");
+                break;
+            }
+            ExitCode::InvalidOperation => {
+                println!("Computer encountered invalid operation");
+                break;
+            }
+            ExitCode::OutOfOperations => {
+                println!("Computer ran out of operations");
+                break;
+            }
+            ExitCode::NeedInput => match get_line() {
+                Ok(line) => comp.add_input(&line),
+                Err(_) => println!("Something went wrong, try again?"),
+            },
+        }
     }
 
-    write("output.txt", comp.get_output())?;
+    // print!("{}", comp.get_output_as_string());
+    // write("output.txt", comp.get_output());
+}
 
-    Ok(())
+fn get_line() -> Result<Vec<u8>> {
+    let mut line = "".to_string();
+    stdin().read_line(&mut line)?;
+    Ok(line.trim_end().try_into()?)
 }
